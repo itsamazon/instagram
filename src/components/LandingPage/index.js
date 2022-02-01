4c6de0f097b261b9e826f5b96ed9ca1e7a042e76
chidera <chidera.diandra@gmail.com>
2022-02-01 20:17:51 +0100

    Project setup

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,13 @@
+
+
+const LandingPage =() => {
+    return(
+        <div>
+            <h1>Welcome to Instagram</h1>
+            <p>Chinny bido oru !!</p>
+        </div>
+        
+    )
+}
+
+export default LandingPage;
